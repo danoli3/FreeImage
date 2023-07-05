0115f955ecf53e47ba5b3d7f9a0b3a6cb3959fdc
Dan Rosser <danoli3@gmail.com>
2023-07-06 01:43:04 +1000

    Fix LibPNG

--- Source/LibPNG/pngpriv.h
+++ Source/LibPNG/pngpriv.h
@@ -127,14 +127,13 @@
     * associated assembler code, pass --enable-arm-neon=no to configure
     * or put -DPNG_ARM_NEON_OPT=0 in CPPFLAGS.
     */
-// Hard defined out for now
-// #  if (defined(__ARM_NEON__) || defined(__ARM_NEON)) && \
-//    defined(PNG_ALIGNED_MEMORY_SUPPORTED)
-// #     define PNG_ARM_NEON_OPT 2
-// #  else
- #     define PNG_ARM_NEON_OPT 0
-// #  endif
-// #endif
+#  if (defined(__ARM_NEON__) || defined(__ARM_NEON)) && \
+   defined(PNG_ALIGNED_MEMORY_SUPPORTED)
+#     define PNG_ARM_NEON_OPT 2
+#  else
+#     define PNG_ARM_NEON_OPT 0
+#  endif
+#endif
 
 #if PNG_ARM_NEON_OPT > 0
    /* NEON optimizations are to be at least considered by libpng, so enable the
